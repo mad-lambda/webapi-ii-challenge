a035361c0bda6844cb26338b3e479bcac5b17f74
Marc de la Torre <mdelatorre1314@gmail.com>
2019-05-24 18:45:38 -0700

    Get endpoint for /api/posts created and tested using postman

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
-
+const db = require('./data/db.js')
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>API</h>
-    <p>Welcome to the API</p>
+    <h2>Posts API</h>
+    <p>Welcome to the Posts API</p>
   `);
 });
+
+server.get('/api/posts', async (req, res) => {
+  try {
+    const posts = await db.find(req.query);
+    res.status(200).json(posts);
+  } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Error retrieving posts"
+      });
+    } 
+  });
 
 module.exports = server;
