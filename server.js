ee49ace41789279e7da4bd7c6fb2e17706e3055f
Marc de la Torre <mdelatorre1314@gmail.com>
2019-05-24 18:57:12 -0700

    Updated Get requests with correct specifications from README.md

--- server.js
+++ server.js
@@ -18,9 +18,26 @@ server.get('/api/posts', async (req, res) => {
   } catch (err) {
       console.log(err);
       res.status(500).json({
-        message: "Error retrieving posts"
+        error: "The posts information could not be retrieved."
       });
     } 
+  });
+
+  server.get('/api/posts/:id', async (req, res) => {
+    try {
+      const post = await db.findById(req.params.id);
+  
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      });
+    }
   });
 
 module.exports = server;
